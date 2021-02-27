682c55b0bd96675b5e489d65c8082419d31e1254
Euler Dias <eulerdias@ciandt.com>
2021-02-27 11:57:36 -0300

    [store] - configuração de store e persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,30 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersistence from "vuex-persist";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+export class RootState {}
+
+const vuexLocal = new VuexPersistence<RootState>({
+  key: "app-filmes-series",
+  storage: window.localStorage
+})
+
+const store = new Vuex.Store<{}>({
+  modules: {
+    filmes: {},
+    series: {},
+  },
+  plugins: [vuexLocal.plugin]
 });
+
+Object.assign(window, { $store: store });
+
+export default store;
+export const vxm = {
+  localStorage: {
+    filme: {},
+    serie: {}
+  },
+}
