49a448d0f96649761e05e25dd95700d0b4f3e53e
Euler Dias <eulerdias@ciandt.com>
2021-02-28 07:40:24 -0300

    [menu] - Alteração nos icones da toolbar e inicio de configuracao do menu

---
+++ src/features/_crosscutting/components/unicos/toolbar/menuPrincipal.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-list nav id="menu-principal">
+    <v-list-group :value="true" prepend-icon="mdi-account-circle">
+      <template v-slot:activator>
+        <v-list-item-title>Filmes</v-list-item-title>
+      </template>
+    </v-list-group>
+    <v-list-item v-for="([titulo, link], i) in menus" :key="i" link>
+      <v-list-item-title v-text="titulo" :to="link"></v-list-item-title>
+    </v-list-item>
+  </v-list>
+</template>
+<style scoped></style>
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+@Component({})
+export default class MenuPrincipal extends Vue {}
+</script>
